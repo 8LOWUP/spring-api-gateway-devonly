2196e6b900066ffffc8eef40c6c1315755f5e08a
shimfff <shimseawon1510@gmail.com>
2025-09-29 16:11:34 +0900

    [feat] : 모든 Origin 허용

--- src/main/java/com/example/gatewayservice/config/CorsConfig.java
+++ src/main/java/com/example/gatewayservice/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트
-                .allowedOrigins("http://localhost:3000", "https://example.com") // 허용할 Origin
+                .allowedOrigins("*") // 허용할 Origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
