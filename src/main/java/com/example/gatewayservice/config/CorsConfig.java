27fbdb38015384210bf331ed8546cc6b01b70bbf
shimfff <shimseawon1510@gmail.com>
2025-09-29 16:10:46 +0900

    [feat] : webflux용 cors 설정 추가

--- src/main/java/com/example/gatewayservice/config/CorsConfig.java
+++ src/main/java/com/example/gatewayservice/config/CorsConfig.java
@@ -2,17 +2,17 @@ package com.example.gatewayservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebFluxConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        registry.addMapping("/**") // 모든 엔드포인트
+                .allowedOrigins("http://localhost:3000", "https://example.com") // 허용할 Origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .maxAge(3600);
+                .allowCredentials(true);
     }
 }
